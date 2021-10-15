bc0a7009349e0d54cdb7f6eddd90e6fb690fb638
annasahu <annagsahu@gmail.com>
2021-10-14 21:17:48 -0300

    input field + operations

--- prova01/lib/main.dart
+++ prova01/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Tela(),
   ));
 }
@@ -14,65 +15,108 @@ class Tela extends StatefulWidget {
 }
 
 class _TelaState extends State<Tela> {
+  final TextEditingController _controllerNum1 = TextEditingController();
+  final TextEditingController _controllerNum2 = TextEditingController();
+
   String option = " ";
-  var num1 = 0.0;
-  var num2 = 0.0;
+  double sum = 0.0;
+  double sub = 0.0;
+  double mul = 0.0;
+  double div = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora"),
+        title: const Text("Calculadora"),
         backgroundColor: Colors.pink.shade300,
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            RadioListTile(
-              title: Text("Soma"),
-              value: "soma",
-              groupValue: option,
-              onChanged: (String? value) {
-                setState(() {
-                  option = value!;
-                });
-              },
+      body: Column(
+        children: <Widget>[
+          // ENTRADAS DE DADOS
+          TextField(
+            controller: _controllerNum1,
+            autofocus: true,
+            decoration: const InputDecoration(
+              labelText: "Digite um número",
+              border: UnderlineInputBorder(),
             ),
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+          ),
 
-            RadioListTile(
-              title: Text("Subtração"),
-              value: "subtracao",
-              groupValue: option,
-              onChanged: (String? value) {
-                setState(() {
-                  option = value!;
-                });
-              },
+          TextField(
+            controller: _controllerNum2,
+            autofocus: true,
+            decoration: const InputDecoration(
+              labelText: "Digite um número",
+              border: UnderlineInputBorder(),
             ),
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+          ),
 
-            RadioListTile(
-              title: Text("Multiplicação"),
-              value: "multiplicacao",
-              groupValue: option,
-              onChanged: (String? value) {
-                setState(() {
-                  option = value!;
-                });
-              },
-            ),
+          // RADIO BUTTONS
+          RadioListTile(
+            title: const Text("Soma"),
+            value: "soma",
+            groupValue: option,
+            onChanged: (String? value) {
+              setState(() {
+                option = value!;
+                sum = double.parse(_controllerNum1.text) +
+                    double.parse(_controllerNum2.text);
+                print(sum);
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text("Subtração"),
+            value: "subtracao",
+            groupValue: option,
+            onChanged: (String? value) {
+              setState(() {
+                option = value!;
+                sub = double.parse(_controllerNum1.text) -
+                    double.parse(_controllerNum2.text);
+                print(sub);
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text("Multiplicação"),
+            value: "multiplicacao",
+            groupValue: option,
+            onChanged: (String? value) {
+              setState(() {
+                option = value!;
+                mul = double.parse(_controllerNum1.text) *
+                    double.parse(_controllerNum2.text);
+                print(mul);
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text("Divisão"),
+            value: "divisao",
+            groupValue: option,
+            onChanged: (String? value) {
+              setState(() {
+                option = value!;
+                div = double.parse(_controllerNum1.text) /
+                    double.parse(_controllerNum2.text);
+                print(div);
+              });
+            },
+          ),
 
-            RadioListTile(
-              title: Text("Divisão"),
-              value: "divisao",
-              groupValue: option,
-              onChanged: (String? value) {
-                setState(() {
-                  option = value!;
-                });
-              },
-            ),
-          ],
-        ),
+          //OUTPUT
+          
+        ],
       ),
     );
   }
